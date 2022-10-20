cf807b274886ba284a39b09ef0ffcaca5e48dab7
Алиса <Andreyadad@gmail.com>
2022-10-20 12:10:31 +0500

    Сделал изменения в main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log();
+let Coffee = {
+    Price: 540,
+    Intensity: {
+        CoffeeIntensityByPackage: '2',
+        maxCoffeeIntensity: '5'
+    }
+};
+
+console.log(typeof Coffee.Price);
+console.log(Coffee);
+console.log(Coffee.Intensity.CoffeeIntensityByPackage);

78417b0a468d0350857aab4ef487f7246a259ecb
Андрей <Andreyadad@gmail.com>
2022-11-26 13:32:24 +0500

    Убрал закомментированный кусок кода

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-/*let Coffee = {
-    Price: 540,
-    Intensity: {
-        CoffeeIntensityByPackage: 2,
-        maxCoffeeIntensity: 5
-    }
-};
-
-let input = document.querySelector('input');
-
-console.log(typeof Coffee.Price);
-console.log(Coffee);
-console.log(Coffee.Intensity.CoffeeIntensityByPackage);
-console.log(input.value);*/
-
 var GAP = 10;
 var FONT_GAP = 15;
 var BAR_HEIGHT = 20;
